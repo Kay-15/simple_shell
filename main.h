32fc35259f500bc42b4f71172a2482fda06c8a94
kay-15 <rnkhangweni@gmail.com>
2023-08-22 13:03:29 -0700

    first few file

---
+++ main.h
@@ -0,0 +1,28 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <sys/stat.h>
+/*function prototypes*/
+char **get_path_dirs(char **en);
+void messsage_error(char *name, int cycles, char **cmd);
+void execute_command(char **command, char **name, char **env, int cycles);
+void print_environ(char **env);
+int string_comapre(char *str1, char *str2);
+unsigned int string_len(char*str);
+char *string_con(char *destination, char *source);
+char *string_copy(char *destination ,char *source);
+int str_int(char *string);
+void handle_signal(int promt_handle);
+void handle_EOF(char *buffer_string);
+void free_memory(char **cmd);
+void free_mem_exist(char **cmd);
+void exist_shell(char **cmd);
+void child_pro (char **name, char **en, int cycles);
+int change_directory(const char *path);
+char **tokenizer(char *buffer,*buffer, const char *string);
+#endif
